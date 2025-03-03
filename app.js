991fdae8147e452b70f975c33588438e9a41916a
NandaMelooo <mfmelo.de@gmail.com>
2025-03-03 15:26:06 -0300

    Atualizando o código para melhor praticidade

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML= "Jogo Numero Secreto 2.0"
+//let titulo = document.querySelector('h1');
+//titulo.innerHTML= "Jogo Numero Secreto 2.0"
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = "Escolha um número entre 1 e 10:";
+//let paragrafo = document.querySelector('p');
+//paragrafo.innerHTML = "Escolha um número entre 1 e 10:";
+
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML= texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo Numero Secreto 2.0');
+exibirTextoNaTela('p','Escolha um número entre 1 e 10:');
 
 function verificarChute(){
      console.log('Botão foi clicado!')
