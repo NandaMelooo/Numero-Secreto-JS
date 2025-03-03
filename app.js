028b363238fda84931ffbba7a3ff647c3e40dba8
NandaMelooo <mfmelo.de@gmail.com>
2025-03-03 15:13:43 -0300

    Adicionando primeira função

--- app.js
+++ app.js
@@ -2,4 +2,8 @@ let titulo = document.querySelector('h1');
 titulo.innerHTML= "Jogo Numero Secreto 2.0"
 
 let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = "Escolha um número entre 1 e 10:"
+paragrafo.innerHTML = "Escolha um número entre 1 e 10:";
+
+function verificarChute(){
+     console.log('Botão foi clicado!')
+}
