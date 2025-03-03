a4261d55ea7946494db6969a0e19012885bc86c6
NandaMelooo <mfmelo.de@gmail.com>
2025-03-03 15:08:32 -0300

    Usando querySelector e innerHTML

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 let titulo = document.querySelector('h1');
 titulo.innerHTML= "Jogo Numero Secreto 2.0"
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = "Escolha um número entre 1 e 10:"
